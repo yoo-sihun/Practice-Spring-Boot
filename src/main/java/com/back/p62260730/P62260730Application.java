32d11b2083933f306c38b476c6994b631ce7746f
yosi475 <yosi475@naver.com>
2026-07-30 11:23:39 +0900

    setting : gitignore & datasource

---
+++ src/main/java/com/back/p62260730/P62260730Application.java
@@ -0,0 +1,13 @@
+package com.back.p62260730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P62260730Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P62260730Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/p62260730/P62260730ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p62260730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P62260730ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

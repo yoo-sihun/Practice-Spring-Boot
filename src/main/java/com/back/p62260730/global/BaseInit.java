76cd2b70eec61bb022a0ad946068bc5f47c36f52
yosi475 <yosi475@naver.com>
2026-07-31 09:29:33 +0900

    change file form & add @Autowired postRepository

--- src/main/java/com/back/p62260730/global/BaseInit.java
+++ src/main/java/com/back/p62260730/global/BaseInit.java
@@ -1,5 +1,7 @@
 package com.back.p62260730.global;
 
+import com.back.p62260730.domain.post.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 빈 등록용
 public class BaseInit {
 
+    @Autowired
+    private PostRepository postRepository;
     @Bean
     public ApplicationRunner init() {
         return args -> {
-            System.out.println("초기 데이터를 로딩합니다.");
+            System.out.println("post의 row 개수를 셉니다");
+            postRepository.count(); // select count(*) from post;
         };
     }
 }

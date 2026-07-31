68218d389dda664220f95c1fff58517916004ae4
yosi475 <yosi475@naver.com>
2026-07-31 10:23:06 +0900

    delete @RequireArgsConstructure & use constructure

--- src/main/java/com/back/p62260730/domain/post/entity/Post.java
+++ src/main/java/com/back/p62260730/domain/post/entity/Post.java
@@ -5,13 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter // 어노테이션. 표식(자바, 프레임워크, IDE)
 @Entity
-@RequiredArgsConstructor
+@NoArgsConstructor
 // 롬복은 모두 받는 AllArgsConstructor랑 NoArgsConstructor만 만들어 줄 수있다
 // 아니면 final이라는 표식을 남기면 이걸 골라서 받을 수 있는 @RequireArgsConstructor있다
 // 단순히 생성자 만들기 쉽게 하기 위해 final 붙이면 안된다
@@ -19,12 +19,13 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // int
-    private final String title; //varchar(255)
-    private final String body; //varchar(255)
+    private String title; //varchar(255)
+    private String body; //varchar(255)
     // 엔티티 구조를 바꿀경우 ddl-auto 사용중일땐 파일 db파일 지웠다가 다시 rerun하기
 
-    public Post() {
-        title = "";
-        body = "";
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
     }
 }
--- src/main/java/com/back/p62260730/global/BaseInit.java
+++ src/main/java/com/back/p62260730/global/BaseInit.java
@@ -23,9 +23,7 @@ public class BaseInit {
             }
             postRepository.count(); // select count(*) from post;
             // post 하나 저장
-            Post post = new Post();
-            post.setTitle("제목1");
-            post.setBody("내용1");
+            Post post = new Post("제목1", "내용1");
             postRepository.save(post);
             // insert into post ...
 

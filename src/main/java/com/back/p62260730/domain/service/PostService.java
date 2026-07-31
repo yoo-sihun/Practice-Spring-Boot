98ed79ec3f6bbf44581448dd85d2cdaa6e51b365
yosi475 <yosi475@naver.com>
2026-07-31 12:25:21 +0900

    use @Transactional and readOnly = true

--- src/main/java/com/back/p62260730/domain/service/PostService.java
+++ src/main/java/com/back/p62260730/domain/service/PostService.java
@@ -5,21 +5,30 @@ import com.back.p62260730.domain.post.entity.Post;
 import com.back.p62260730.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Component
+@Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
-
+// 트랜잭션 범위는 jpa가 제공하는 메서드 단위로 트랜잭션 기능이 잡힌다.
+    //jpa -> jakarta
+    // @Transactional은 spring패키지 사용
     public Post write(String title, String body) {
         Post post = new Post(title, body);
-        return postRepository.save(post);
+        return postRepository.save(post); // insert into .. sql문 발생 => sql문이 db에 반영될려면 commit
+        // 기본적으로 jpa가 제공하는 기능은 그 자 체로 트랜잭션 범위를 가진다.
+    }
+
+    public void delete(Post post) {
+        postRepository.delete(post);
     }
 
     public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+        return postRepository.findById(id); // 트랜잭션 시작 ->  select * from post -> 트랜잭션 종료 -> 커밋
     }
 
     public long count() {
--- src/main/java/com/back/p62260730/global/BaseInit.java
+++ src/main/java/com/back/p62260730/global/BaseInit.java
@@ -1,18 +1,23 @@
 package com.back.p62260730.global;
 
-import com.back.p62260730.domain.post.repository.PostRepository;
+import com.back.p62260730.domain.post.entity.Post;
 import com.back.p62260730.domain.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration // 빈 등록용
+@RequiredArgsConstructor
 public class BaseInit {
 
+    private final PostService postService; // 창고관리인
     @Autowired
-    private PostRepository postRepository;
-    private PostService postService; // 창고관리인
+    @Lazy
+    private BaseInit self;
 
     @Bean
     public ApplicationRunner init() {
@@ -20,9 +25,14 @@ public class BaseInit {
             System.out.println("초기화 작업을 수행합니다.");
             work1();
             work2();
+
+            new Thread(() -> {
+                self.work3();
+            }).start();
+
         };
     }
-
+    @Transactional
     void work1() {
 
             if(postService.count() > 0) {
@@ -30,13 +40,30 @@ public class BaseInit {
             }
 
             postService.write("제목1", "내용1");
+
+            if(true) {
+                throw new RuntimeException("테스트 예외");
+            }
             postService.write("제목2", "내용2");
 
 
         }
-
+        @Transactional(readOnly = true) // 조회용 메서드는 db변경을 하지 않는다
         void work2() {
             postService.findById(1);
             // select * from post where id = 1;
         }
+
+    @Transactional
+    void work3() {
+        Post post1 = postService.findById(1).get();
+        Post post2 = postService.findById(2).get();
+
+        postService.delete(post1);
+
+        if(true) {
+            throw new RuntimeException("테스트 예외");
+        }
+        postService.delete(post2);
+    }
     }
